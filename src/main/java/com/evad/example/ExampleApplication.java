c123eb9de244b4ea3e4dde0a5768fbfe26aefea0
EvgheniAdvahov <evghenii.88@gmail.com>
2024-03-10 20:20:59 +0200

    REST

--- src/main/java/com/evad/example/ExampleApplication.java
+++ src/main/java/com/evad/example/ExampleApplication.java
@@ -1,18 +1,14 @@
 package com.evad.example;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(ExampleApplication.class, args);
+		SpringApplication.run(ExampleApplication.class, args);
 
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.teeAStory());
 	}
 
 
